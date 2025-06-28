f80aba4cd0d5d95171661a3745436b92b05adad7
Bhagavan11 <bhagavan9182@gmail.com>
2025-06-28 09:52:31 +0530

    Check: working production build

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,42 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
-const app = express();
-const port = process.env.PORT || 5001;
 
-console.log("🚀 App starting...");
-app.use(express.json({ limit: "10mb" })); // Or more if needed
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-app.get("/", (req, res) => {
-  console.log("🟢 GET / request received");
-  res.send("hello world");
-});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages",messageRoutes)
+app.use("/api/messages", messageRoutes);
 
-connectDB()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`✅ Server is running at http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error("❌ DB Connection Failed:", err.message);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
