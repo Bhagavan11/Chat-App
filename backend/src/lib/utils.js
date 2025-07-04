4efd4b3e6da4b1d7942b08e15e00260cf4f8ded9
Bhagavan11 <bhagavan9182@gmail.com>
2025-07-04 15:37:14 +0530

    utiljs modified

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,19 +1,16 @@
-import jwt from "jsonwebtoken";
-
 export const generateToken = (userId, res) => {
-  console.log("generate token triggered")
+  console.log("generate token triggered");
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  console.log(token)
+  console.log(token);
 
-  var x=res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    sameSite: "None", // ✅ must be None for cross-origin
+    secure: process.env.NODE_ENV !== "development", // ✅
   });
-  console.log(x)
 
   return token;
 };
