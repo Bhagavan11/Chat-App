63f62ab1598f4b1f0bf5aa23bc0de62957f32b6c
Bhagavan11 <bhagavan9182@gmail.com>
2025-07-04 16:08:50 +0530

    modified socketjs

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,9 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://chat-app-snowy-eta.vercel.app"],
+    credentials: true,
   },
 });
+
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
