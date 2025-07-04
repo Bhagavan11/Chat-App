ac8f8f5c355a13a3c95063894546d237928eac2a
Bhagavan11 <bhagavan9182@gmail.com>
2025-07-04 15:42:25 +0530

    utiljs modified1

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 export const generateToken = (userId, res) => {
   console.log("generate token triggered");
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
